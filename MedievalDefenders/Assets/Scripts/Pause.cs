7ebcfa7b7de2ea78389ad62217d0be9b04af7245
Bernardo Lara <be1212lara@gmail.com>
2023-10-04 22:38:19 -0300

    pause

---
+++ MedievalDefenders/Assets/Scripts/Pause.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public Transform pause;
+    [SerializeField] private GameObject painelOpcoes;
+    [SerializeField] private string mainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(pause.gameObject.activeSelf){
+                pause.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }else{
+                pause.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+    public void RetornarGame(){
+        pause.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void SairGame(){
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    public void OpcoesGame(){
+		pause.gameObject.SetActive(false);
+		painelOpcoes.SetActive(true);
+    }
+}
