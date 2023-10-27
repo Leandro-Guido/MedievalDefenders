92c47e6af9b717cdff33b4027bb2bc13727a28a8
Leandro Guido <leandroguidos@gmail.com>
2023-10-27 10:41:55 -0300

    novo grafos

--- MedievalDefenders/Assets/_Scripts/Units/Enemy/EnemyMovement.cs
+++ MedievalDefenders/Assets/_Scripts/Units/Enemy/EnemyMovement.cs
@@ -14,7 +14,7 @@ public class EnemyMovement : MonoBehaviour
 
     public enum Algorithm
     {
-        DFS,
+        LongestPath,
         dikjstra
     }
 
@@ -62,26 +62,26 @@ public class EnemyMovement : MonoBehaviour
     private void Start()
     {
         int[,] graph = new int[,] {
-            { 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 15, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13, 0 },
-            { 0, 0, 0, 0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 1, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 3, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 7, 13, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 6, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 16, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 0 },
+            { 0, 0, 0, 0, 8, 9, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 4, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 8, 14, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         }; // Grafo inicial projetado anteriormente
 
         int [] shortestDistances;
-        if (_algorithm == Algorithm.DFS)
+        if (_algorithm == Algorithm.LongestPath)
         {
-            shortestDistances = Graphs.DFSWithStop(0, 13, graph); // Caminho dos inimigos usando a Busca em Profundidade
+            shortestDistances = Graphs.FindLongestPath(0, 13, graph); // Caminho dos inimigos usando a Busca em Profundidade
         }
         else
         {
