1c86dc777923d8174e42202873a882c0949e3395
Leandro Guido <leandroguidos@gmail.com>
2023-11-22 22:04:21 -0300

    AStar

--- MedievalDefenders/Assets/_Scripts/Util/Graphs.cs
+++ MedievalDefenders/Assets/_Scripts/Util/Graphs.cs
@@ -69,6 +69,79 @@ public class Graphs : MonoBehaviour
         return pathArray;
     }
 
+    static public int[] AStar(int source, int destination, int[,] graph)
+    {
+        int V = graph.GetLength(0); // Obtém o número de vértices no grafo.
+        int[] dist = new int[V]; // Array para armazenar as distâncias mínimas.
+        int[] parent = new int[V]; // Array para rastrear os pais no caminho mínimo.
+        bool[] visited = new bool[V]; // Array para rastrear os nós visitados.
+        List<float> heuristicas = SingletonExample.Instance.heuristics; // Array com as heurísticas
+
+        // Inicializa os arrays de distância, pai e visitados.
+        for (int i = 0; i < V; i++)
+        {
+            dist[i] = int.MaxValue; // Define a distância inicial para infinito.
+            visited[i] = false; // Marca todos os nós como não visitados.
+        }
+
+        dist[source] = 0; // A distância do nó de origem para ele mesmo é zero.
+        parent[source] = -1; // Define o pai do nó de origem como -1, indicando que é o nó de início.
+
+        for (int count = 0; count < V - 1; count++)
+        {
+            int u = -1;
+
+            // Encontra o nó não visitado com a menor distância.
+            for (int i = 0; i < V; i++)
+            {
+                if (!visited[i] && u == -1)
+                {
+                    u = i;
+                }
+                if (u > -1)
+                {
+                    if (!visited[i] && (dist[i] + heuristicas[i] < dist[u] + heuristicas[u]))
+                    {
+                        u = i;
+                    }
+                }
+            }
+
+            visited[u] = true; // Marca o nó u como visitado.
+
+            // Atualiza as distâncias e os pais dos nós vizinhos.
+            for (int v = 0; v < V; v++)
+            {
+                if (!visited[v] && graph[u, v] != 0 && dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+                {
+                    dist[v] = dist[u] + graph[u, v];
+                    parent[v] = u;
+                }
+            }
+        }
+
+        // Cria uma lista para armazenar o caminho mínimo.
+        List<int> pathList = new List<int>();
+        int current = destination;
+
+        // Reconstrói o caminho mínimo a partir do destino até o ponto de origem.
+        while (current != -1)
+        {
+            pathList.Insert(0, current); // Insere o nó atual no início da lista.
+            current = parent[current]; // Move para o nó pai no caminho.
+        }
+
+        // Converte a lista em um vetor e retorna o caminho mínimo.
+        int[] pathArray = pathList.ToArray();
+
+        for (int i = 0; i < pathArray.Length; i++)
+        {
+            pathArray[i] = (pathArray[i] + 1); // Transformar vertice 0 em 1 e assim por diante 
+        }
+
+        return pathArray;
+    }
+
     static public int[] DFSWithStop(int source, int destination, int[,] graph)
     {
         int V = graph.GetLength(0); // Obtém o número de vértices do grafo.
